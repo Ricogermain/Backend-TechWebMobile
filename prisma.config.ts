e2605da1bdbddbdf31b17c9f3accfd7cbe7e520f
Ricogermain <ricogermain500@gmail.com>
2026-07-25 20:53:40 +0300

    migration db

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,8 +7,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    provider: "postgresql",
-    url: env("DATABASE_URL"),
-    directUrl: env("DIRECT_URL"),
+    url: env("DIRECT_URL"),
   },
 });
