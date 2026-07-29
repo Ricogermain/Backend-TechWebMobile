0cc1d7bf61d0ce85eec4c66661f842c048d4db98
Ricogermain <ricogermain500@gmail.com>
2026-07-29 13:29:12 +0300

    mise en place des elementes de base du module 5.3

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,23 @@ import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { VehiculesModule } from './vehicules/vehicules.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule],
+  imports: [
+    //20 requete par min max
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 20 }]),
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    VehiculesModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
   ],
 })
 export class AppModule {}
